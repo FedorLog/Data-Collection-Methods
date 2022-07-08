577e4262a75f917a43cccf5daab11a58622978ed
FedorLog <108396605+FedorLog@users.noreply.github.com>
2022-07-08 03:15:50 -0600

    Delete Lesson - 1 directory

--- Lesson - 1/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-url = 'https://api.github.com'
-user ='FedorLog'
-r = requests.get(f'{url}/users/{user}/repos')
-with open('data.json', 'w') as file:
-    json.dump(r.json(), f)
-for i in r.json():
-    print(i['name'])
